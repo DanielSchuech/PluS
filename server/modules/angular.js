c94440373912dfebfd902ba58a6aec5d45c55746
Daniel Schuech <dschuech@gmail.com>
2015-12-07 14:12:09 +0000

    fix: fonts directory in build

--- server/modules/angular.js
+++ server/modules/angular.js
@@ -6,13 +6,14 @@ module.exports = Angular;
 
 Angular.$inject = ['server', 'config'];
 function Angular(server, config) {
+  var distDir = path.join(config.dist.root, 'plugin_system', 'dist');
+  
   //resolve glyphicons fonts
-  server.get('/fonts/:font', function(req, res) {console.log('A '+req.params.font)
-    res.sendFile('/dist/fonts/' + req.params.font, {root: config.dist.root});
+  server.get('/fonts/:font', function(req, res) {
+    res.sendFile('/fonts/' + req.params.font, {root: distDir});
   });
 
   // Serve index.html for all routes to leave routing up to Angular
-  var distDir = path.join(config.dist.root, 'plugin_system', 'dist');
   server.get('/*', function(req, res) {
     res.sendFile('index.html', {root: distDir});
   });
